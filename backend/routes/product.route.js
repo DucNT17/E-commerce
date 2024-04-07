fe25f2cb8d0ecfaa44edacbf79584fab25c4b5e3
TienDuc <tienducnguyen174@gmail.com>
2024-04-07 15:21:28 +0700

    change product route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const routes = express.Router()
-const ctrls = require('../controllers/product.controller')
+const product = require('../controllers/product.controller')
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 
-routes.post('/', [verifyAccessToken, isAdmin], ctrls.createProduct);
-routes.get('/', ctrls.getProducts);
-routes.put('/ratings', [verifyAccessToken], ctrls.ratings);
+routes.post('/', [verifyAccessToken, isAdmin], product.createProduct);
+routes.get('/', product.getProducts);
+routes.put('/ratings', [verifyAccessToken], product.ratings);
 
 
-routes.put('/:pid', [verifyAccessToken, isAdmin], ctrls.updateProduct);
-routes.delete('/:pid', [verifyAccessToken, isAdmin], ctrls.deleteProduct);
-routes.get('/:pid', ctrls.getProduct);
+routes.put('/:pid', [verifyAccessToken, isAdmin], product.updateProduct);
+routes.delete('/:pid', [verifyAccessToken, isAdmin], product.deleteProduct);
+routes.get('/:pid', product.getProduct);
 
 
 module.exports = routes;
