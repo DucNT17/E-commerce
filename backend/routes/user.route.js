97196492447965ecb4bd331127043ad567dc11ed
TienDuc <tienducnguyen174@gmail.com>
2024-04-07 15:20:52 +0700

    change user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const routes = express.Router()
-const ctrls = require('../controllers/user.controller')
+const user = require('../controllers/user.controller')
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
-routes.post('/register', ctrls.register)
-routes.post('/login', ctrls.login)
-routes.get('/current', verifyAccessToken, ctrls.getCurrent)
-routes.post('/refreshtoken', ctrls.refreshAccessToken)
-routes.get('/logout', ctrls.logout)
-routes.get('/forgotpassword', ctrls.forgotPassword)
-routes.put('/resetpassword', ctrls.resetPassword)
-routes.get('/', [verifyAccessToken, isAdmin], ctrls.getUsers)
-routes.delete('/', [verifyAccessToken, isAdmin], ctrls.deleteUser)
-routes.put('/current', [verifyAccessToken], ctrls.updateUser)
-routes.put('/:uid', [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
+routes.post('/register', user.register)
+routes.post('/login', user.login)
+routes.get('/current', verifyAccessToken, user.getCurrent)
+routes.post('/refreshtoken', user.refreshAccessToken)
+routes.get('/logout', user.logout)
+routes.get('/forgotpassword', user.forgotPassword)
+routes.put('/resetpassword', user.resetPassword)
+routes.get('/', [verifyAccessToken, isAdmin], user.getUsers)
+routes.delete('/', [verifyAccessToken, isAdmin], user.deleteUser)
+routes.put('/current', [verifyAccessToken], user.updateUser)
+routes.put('/:uid', [verifyAccessToken, isAdmin], user.updateUserByAdmin)
 
 module.exports = routes
 
